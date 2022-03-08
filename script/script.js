c4d50244f5cc68bc526652a58b6f4810b4d5e0ec
Amr Mohamed <amoree.mb@gmail.com>
2022-03-08 10:57:53 +0200

    wrote a function to add player markers

--- script/script.js
+++ script/script.js
@@ -1,18 +1,53 @@
+
 const board = (function() {
-    const gameBoard = ["X","O"];
-     function _render() {
-        gameBoard.forEach(play => {
-            let position = document.getElementById(`${gameBoard.indexOf(play)}`)
-            console.log(play)
-            console.log(position)
-                
-         })
-      }
-      
+    const gameBoard = [];
+    const conts = document.querySelectorAll('.cont');
+   
+    function _render(mark,ind) {
+            let position = document.getElementById(`${ind}`)
+            
+                position.textContent = mark
+            
+        }
+    return {_render,gameBoard,conts}
 })();
-const gameBoard = ["X","O"];
-gameBoard.forEach((play, index) => {
-    let position = document.getElementById(`${index}`)
-    position.textContent = play
-    console.log(index)
-})
+
+const Player = mark => {
+   
+    const marker = (ind) => { 
+        board.gameBoard.push(mark);    
+        board._render(mark,ind)
+    }
+    return {
+        marker
+    }
+}
+
+const player1 = Player("X")
+
+const player2 = Player("O")
+
+
+
+const gameFlow = (function() {
+    
+        board.conts.forEach((div,ind) => (div.addEventListener('click', () => {
+            if (div.textContent != "") {
+                return
+            }
+            if (board.gameBoard.length % 2 == 0) {
+                player1.marker(ind)
+                console.log(board.gameBoard)
+                console.log(board.gameBoard.length)
+            }
+            else  {
+                player2.marker(ind)
+                console.log(board.gameBoard)
+                console.log(board.gameBoard.length)
+            }        
+        })))
+    
+    
+
+    
+})();
