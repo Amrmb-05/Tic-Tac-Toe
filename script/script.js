c779749abbd31c5469aca364fea9c1b5cc44d75a
Amr Mohamed <amoree.mb@gmail.com>
2022-03-08 22:28:31 +0200

    added a display element for the winning player

--- script/script.js
+++ script/script.js
@@ -28,6 +28,7 @@ const gameFlow = (function() {
         const player1 = Player("X")
         const player2 = Player("O")
         const container = document.getElementById("container")
+        const winnerDisplay = document.getElementById("winner-display")
         board.conts.forEach((div,ind) => (div.addEventListener('click', () => {
 
             if (div.textContent != "") {
@@ -45,39 +46,40 @@ const gameFlow = (function() {
             }        
         
             if (board.conts[0].textContent == board.conts[1].textContent && board.conts[1].textContent ==  board.conts[2].textContent && board.conts[2].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[0].textContent} won`
                 container.style.pointerEvents = "none"
             }
             else if (board.conts[0].textContent == board.conts[3].textContent && board.conts[3].textContent ==  board.conts[6].textContent && board.conts[6].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[0].textContent} won`
                 container.style.pointerEvents = "none"
                 
             }
             else if (board.conts[0].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[8].textContent && board.conts[8].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[0].textContent} won`
                 container.style.pointerEvents = "none"
             }
             else if (board.conts[2].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[6].textContent && board.conts[6].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[2].textContent} won`
                 container.style.pointerEvents = "none"
             }
             else if (board.conts[3].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[5].textContent && board.conts[5].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[3].textContent} won`
                 container.style.pointerEvents = "none"
             }
             else if (board.conts[1].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[7].textContent && board.conts[7].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[1].textContent} won`
                 container.style.pointerEvents = "none"
             }
             else if (board.conts[6].textContent == board.conts[7].textContent && board.conts[7].textContent ==  board.conts[8].textContent && board.conts[8].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[6].textContent} won`
                 container.style.pointerEvents = "none"
             }
             else if (board.conts[2].textContent == board.conts[5].textContent && board.conts[5].textContent ==  board.conts[8].textContent && board.conts[8].textContent != "") {
-                alert("You won")
+                winnerDisplay.textContent = `${board.conts[2].textContent} won`
                 container.style.pointerEvents = "none"
             }
             if (board.gameBoard.length === 9) {
+                winnerDisplay.textContent = "Tie"
                 alert("tie")
                 container.style.pointerEvents = "none"
             }
