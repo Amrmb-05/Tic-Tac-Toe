f3065ebe957512042edb4087e88ccfa0be8671be
Amr Mohamed <amoree.mb@gmail.com>
2022-03-08 22:17:23 +0200

    wrote the logic for when a player wins a game or a tie

--- script/script.js
+++ script/script.js
@@ -23,15 +23,13 @@ const Player = mark => {
     }
 }
 
-const player1 = Player("X")
-
-const player2 = Player("O")
-
-
 
 const gameFlow = (function() {
-    
+        const player1 = Player("X")
+        const player2 = Player("O")
+        const container = document.getElementById("container")
         board.conts.forEach((div,ind) => (div.addEventListener('click', () => {
+
             if (div.textContent != "") {
                 return
             }
@@ -45,9 +43,47 @@ const gameFlow = (function() {
                 console.log(board.gameBoard)
                 console.log(board.gameBoard.length)
             }        
+        
+            if (board.conts[0].textContent == board.conts[1].textContent && board.conts[1].textContent ==  board.conts[2].textContent && board.conts[2].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+            }
+            else if (board.conts[0].textContent == board.conts[3].textContent && board.conts[3].textContent ==  board.conts[6].textContent && board.conts[6].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+                
+            }
+            else if (board.conts[0].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[8].textContent && board.conts[8].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+            }
+            else if (board.conts[2].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[6].textContent && board.conts[6].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+            }
+            else if (board.conts[3].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[5].textContent && board.conts[5].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+            }
+            else if (board.conts[1].textContent == board.conts[4].textContent && board.conts[4].textContent ==  board.conts[7].textContent && board.conts[7].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+            }
+            else if (board.conts[6].textContent == board.conts[7].textContent && board.conts[7].textContent ==  board.conts[8].textContent && board.conts[8].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+            }
+            else if (board.conts[2].textContent == board.conts[5].textContent && board.conts[5].textContent ==  board.conts[8].textContent && board.conts[8].textContent != "") {
+                alert("You won")
+                container.style.pointerEvents = "none"
+            }
+            if (board.gameBoard.length === 9) {
+                alert("tie")
+                container.style.pointerEvents = "none"
+            }
         })))
     
     
-
+        
     
 })();
